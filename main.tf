9451b01fbf07574025c4288e0d864ccf1bec4330
Ovvio2311 <93138661+Ovvio2311@users.noreply.github.com>
2024-03-16 22:27:42 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,11 +21,11 @@ provider "google" {
   host                   = "https://${module.gke.endpoint}" 
   token                  = data.google_client_config.default.access_token    
   cluster_ca_certificate   = base64decode(module.gke.ca_certificate)
-  /*exec {
+  exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     args        = ["container", "clusters", "get-credentials", var.cluster_name, "--zone", "us-central1", "--project", var.project_id]      
     command     = "gcloud"
-    }*/
+    }
   # client_key             = base64decode(data.google_container_cluster.primary.master_auth.0.client_key)
   # client_certificate = base64decode(data.google_container_cluster.primary.master_auth.0.client_certificate)
 }
