599f5e91a195fb8df6b51441852b1cc47436b6a5
Ovvio2311 <93138661+Ovvio2311@users.noreply.github.com>
2024-04-24 15:18:56 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -354,6 +354,15 @@ resource "google_service_account" "bucket_account" {
   create_ignore_already_exists = true
   
 }
+
+resource "google_project_iam_binding" "bind_user" {
+  project = var.project_id
+  role    = "roles/storage.admin"
+  members = [
+    "serviceAccount:fyp-bucket-access-role@my-project-4108m.iam.gserviceaccount.com"
+  ]
+}
+/*
 resource "google_storage_bucket_iam_binding" "binding" {
   bucket = google_storage_bucket.static.name
   role = "roles/storage.admin"
@@ -361,5 +370,5 @@ resource "google_storage_bucket_iam_binding" "binding" {
     "serviceAccount:fyp-bucket-access-role@my-project-4108m.iam.gserviceaccount.com",
   ]
 }
-
+*/
 
