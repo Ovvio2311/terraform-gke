feada99677b4b16888d865433f623d943f37d3e2
Ovvio2311 <93138661+Ovvio2311@users.noreply.github.com>
2024-04-24 12:30:17 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -369,11 +369,3 @@ resource "google_storage_bucket_iam_policy" "policy" {
   policy_data = data.google_iam_policy.admin.policy_data
 }
 
-resource "google_storage_bucket_iam_binding" "binding" {
-  bucket = google_storage_bucket.static.name
-  role = "roles/storage.admin"
-  members = [
-    "serviceaccount:fyp-bucket-access-role@my-project-4108m.iam.gserviceaccount.com",
-  ]
-  depends_on = [google_service_account.bucket_account]
-}
